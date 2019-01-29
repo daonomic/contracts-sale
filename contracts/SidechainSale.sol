2102f151b31ee5decefbe2d5e816fde8a97e8a7b
0v1se <dev@0v1se.com>
2019-01-29 15:30:30 +0300

    add xpub info to SidechainSale

--- contracts/SidechainSale.sol
+++ contracts/SidechainSale.sol
@@ -5,6 +5,21 @@ import "./roles/OperatorRole.sol";
 import "./UiEvents.sol";
 
 contract SidechainSale is UiEvents, OperatorRole, Sale {
+    event XPubChange(address token, string xpub);
+    mapping(address => string) xpubs;
+
+    function setXPub(address _token, string memory _xpub) onlyOwner public {
+        setXPubInternal(_token, _xpub);
+    }
+
+    function setXPubInternal(address _token, string memory _xpub) internal {
+        xpubs[_token] = _xpub;
+        emit XPubChange(_token, _xpub);
+    }
+
+    function getXPub(address token) view public returns (string memory) {
+        return xpubs[token];
+    }
 
     function onReceive(address _buyer, address _token, uint256 _value, bytes memory _txId) onlyOperator public {
         require(_token != address(0));
