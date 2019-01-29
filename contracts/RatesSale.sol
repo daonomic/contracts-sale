b014c66fb086b4ca327436e3c348a8507be31237
0v1se <dev@0v1se.com>
2019-01-29 13:00:54 +0300

    0.3.1 emit ui events

--- contracts/RatesSale.sol
+++ contracts/RatesSale.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
 import "./Sale.sol";
+import "./UiEvents.sol";
 
-contract RatesSale is Sale {
+contract RatesSale is UiEvents, Sale {
     mapping(address => uint) public rates;
 
     function _getAmount(address _beneficiary, address _token, uint _value) internal returns (uint amount, uint change) {
@@ -16,6 +17,11 @@ contract RatesSale is Sale {
     }
 
     function setRate(address _token, uint _rate) public onlyOwner {
+        if (_rate == 0) {
+            emit RateRemove(_token);
+        } else {
+            emit RateAdd(_token);
+        }
         rates[_token] = _rate;
     }
 }
--- contracts/SidechainSale.sol
+++ contracts/SidechainSale.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.5.0;
 
 import "./Sale.sol";
 import "./roles/OperatorRole.sol";
+import "./UiEvents.sol";
 
-contract SidechainSale is OperatorRole, Sale {
+contract SidechainSale is UiEvents, OperatorRole, Sale {
 
     function onReceive(address _buyer, address _token, uint256 _value, bytes memory _txId) onlyOperator public {
         require(_token != address(0));
+        emit ExternalTx(_txId);
         _purchase(_buyer, _token, _value);
     }
 }
