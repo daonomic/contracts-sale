00d2acccb18bdab2681d852a0a1b25f1f4f3ac1d
0v1se <dev@0v1se.com>
2019-02-08 11:54:58 +0300

    tests

--- test/contracts/SimpleFixedPools.sol
+++ test/contracts/SimpleFixedPools.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.5.0;
 
 
-import "../../contracts/Pools.sol";
+import "../../contracts/FixedPools.sol";
 
 
-contract SimplePools is Pools {
-    constructor(ERC20Mintable token, uint floatingStart, uint fixedStart) Pools(token) public {
+contract SimpleFixedPools is FixedPools {
+    constructor(ERC20Mintable token, uint fixedStart) FixedPools(token) public {
         registerPool("Direct", 1000000 * 10 ** 8, 0, ReleaseType.Direct);
-        registerPool("Floating", 1000000 * 10 ** 8, floatingStart, ReleaseType.Floating);
         registerPool("Fixed", 1000000 * 10 ** 8, fixedStart, ReleaseType.Fixed);
     }
 }
